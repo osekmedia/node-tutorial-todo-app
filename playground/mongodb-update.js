32a414930c7abb2b3520be069f27f4464677f182
osekmedia <osekmedia@gmail.com>
2018-07-01 20:29:19 -0700

    adding update demo script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,56 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect( 'mongodb://localhost:27017/TodoApp', (err, db)=>{
+
+	if(err){
+		return console.log('Unable to connect to mongo database server');
+	}
+
+	console.log('Connected to mongoDB server');
+
+	// db.collection('Todos').findOneAndUpdate(
+	// 	{
+	// 		_id: new ObjectID('5b3994c03e1e78e36eff5279')
+	// 	},
+	// 	{
+	// 		$set: {
+	// 			completed: true
+	// 		}
+	// 	},
+	// 	{
+	// 		returnOriginal: false
+	// 	}
+	// ).then( result => {
+	// 	console.log(result);
+	// });
+
+
+	db.collection('Users').findOneAndUpdate(
+		{
+			name: 'Erin'
+		},
+		{
+			$set: {
+				name: 'Erin Duvall'
+			},
+			$inc: {
+				age: 1
+			}
+
+		},
+		{
+			returnOriginal: false
+		}
+	).then( result => {
+		console.log(result);
+	});
+
+	//db.close();
+});
+
+// {
+//     text: 'eat lunch',
+//     completed: false
+// }

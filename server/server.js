9b37e4de8784f9c49fc32f666d1e6cb761328ac8
osekmedia <osekmedia@gmail.com>
2018-07-02 01:49:17 -0700

    Setting up mocha and testing framework

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ var app = express();
 app.use( bodyParser.json() );
 
 app.post('/todos', (req, res) => {
-	
+
 	var todo = new Todo({
 		text: req.body.text
 	});
@@ -27,3 +27,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
 	console.log('Started on port 3000')
 });
+
+module.exports = {app};

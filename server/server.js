6f25466d5d0b77a95312b1cb403c276b8bb82c34
osekmedia <osekmedia@gmail.com>
2018-07-02 21:12:38 -0700

    Adding get todos list route

--- server/server.js
+++ server/server.js
@@ -24,8 +24,17 @@ app.post('/todos', (req, res) => {
 
 });
 
+app.get('/todos', (req, res)=>{
+	//Find all todos and return them
+	Todo.find().then( (todos)=>{
+		res.send({todos}); //use object to hold array for flexibility
+	}, (e)=>{
+		res.status(400).send(e);
+	});
+});
+
 app.listen(3000, () => {
-	console.log('Started on port 3000')
+	console.log('Started on port 3000');
 });
 
 module.exports = {app};
